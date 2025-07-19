96e984d51ae75747d5937f98568213fa06c5a30c
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-19 22:51:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ ADMIN = int(os.environ.get("ADMIN", "7654385403"))
 PICS = (os.environ.get("PICS", "https://files.catbox.moe/gyklg0.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
-NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "-1001457313028").lower() == "True"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://Kafka:Au3OoWzCDYJKeuHU@cluster0.lz2m8iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "cluster0")
 
-IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
+IS_FSUB = os.environ.get("IS_FSUB", "-1001457313028").lower() == "True"  # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002456565415").split())) # Add Multiple channel ids
