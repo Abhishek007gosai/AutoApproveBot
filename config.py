4eecfef6ad77f7172f12696b49eb9404de8197c1
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-20 00:12:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from typing import List
 API_ID = os.environ.get("API_ID", "29245477")
 API_HASH = os.environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", "7812674101"))
+ADMIN = int(os.environ.get("ADMIN", "7654385403"))
 PICS = (os.environ.get("PICS", "https://files.catbox.moe/gyklg0.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
