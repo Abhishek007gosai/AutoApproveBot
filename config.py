0610cfdcca3b65e7d6dce3609c2cfd6c06a3b4df
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-10-30 18:50:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ from typing import List
 
 API_ID = os.environ.get("API_ID", "29245477")
 API_HASH = os.environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8040464811:AAG-mFQbF0KrngzJZHS7dFw0amlikGCMsBc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", "7654385403"))
 PICS = (os.environ.get("PICS", "https://files.catbox.moe/7iqqsb.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://nier88881:8qLsalGiPAvbwKTP@cluster0.od2tq6h.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "cluster0")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
